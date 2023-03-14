c963ca3beb242c638501f75e3543e1e378a123a6
Ramin RX7 <rawmin.rx@gmail.com>
2023-03-15 03:01:53 +0330

    added extract_tasks_category funtion

--- src/functions.ts
+++ src/functions.ts
@@ -4,6 +4,7 @@ import * as fs from "fs"
 
 
 import {getConfigs} from './configs';
+import { TAGS } from './tags';
 var config = getConfigs()
 
 
@@ -58,4 +59,59 @@ function get_requested_tags(){
         }
     });
     console.log(found_lines);
+}
+
+function extract_tasks_category(){
+    let editor = vscode.window.activeTextEditor;
+    let lines = editor.document.getText().split("\n");
+
+    let TAGS_DICT = {};
+    TAGS.forEach(tag => {
+        TAGS_DICT[tag] = []
+    });
+
+    let module_regex = /\[(.+)\](\s*)?/g;
+    let modules_tasks = {}  // MODULE1 : [TASK1,TASK2]
+    let current_module = undefined
+    for (var [index, line] of lines.entries()) {
+        // console.log(index);
+        if (module_regex.exec(line)){
+            line = line.trim().slice(1,line.length-2)
+            modules_tasks[line] = []
+            current_module = line;
+
+        } else if (line.trim()) {
+            line = line.trim()
+            if (Object.values(config.tasksSymbols).includes(line[0])){
+                line = line.slice(1)
+            }
+            let tag_regex = /(?<withspace>\s*(?<TAG>@(?<TAGNAME>[^(\r| |\n|`|'|"|@)]+))\s*)/;
+            let match;
+            let tagname;
+            let line_tagname_list = [];
+            let line_tag_regexed = [];
+            let modified_line = line;
+            while (true) {
+                match = tag_regex.exec(modified_line)
+                if (! match){break}
+                tagname = match.groups.TAGNAME
+                if (TAGS_DICT[tagname]==undefined){
+                    TAGS_DICT[tagname] = []
+                }
+                line_tagname_list.push(tagname)
+                line_tag_regexed.push(match.groups.withspace)
+                modified_line = modified_line.replace(match.groups.withspace, "")
+                // TAGS_DICT[tagname].push(line)
+            }
+            line_tag_regexed.forEach(regexed => {
+                line = line.replace(regexed, "")
+            });
+            line_tagname_list.forEach(tagname => {
+                TAGS_DICT[tagname].push(line.trim())
+            });
+            modules_tasks[current_module].push(line)
+        }
+    }
+    // console.log(modules_tasks);
+    // console.log(TAGS_DICT);
 }
